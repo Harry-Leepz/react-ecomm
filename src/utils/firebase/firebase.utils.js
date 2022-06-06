226699bcea6ecbbaa70b8cc5ab20cf0baf11e2b4
Harry <hdhillon478@gmail.com>
2022-06-06 14:12:08 +0100

    Init firestore, generate userDoc reference

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,6 +8,10 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+// Firestore imports for getting documents and
+// getting and setting document data
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCdJCOleSLlK9hY53QT3oS4DiRALvidBLw",
@@ -28,3 +32,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// Initialize Firestore
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
