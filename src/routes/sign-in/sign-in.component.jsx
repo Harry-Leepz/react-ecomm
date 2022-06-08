54f8645178ada104138a7a9c0684e208b34d3108
Harry <hdhillon478@gmail.com>
2022-06-08 20:57:46 +0100

    Create and set initial state values for signup form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 const SignIn = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
   // Login with Google Popup
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -14,6 +26,8 @@ const SignIn = () => {
     <div>
       <h1>This is the Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google PopUp</button>
+
+      <SignUpForm />
     </div>
   );
 };
