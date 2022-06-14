cbc0916110727426206ba7af7fe16a2d5a7e5516
Harry <hdhillon478@gmail.com>
2022-06-14 21:08:57 +0100

    Build button component with expected props to be received

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,7 +1,17 @@
-const Button = () => {
+const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
   return (
     <div>
-      <button>Button</button>
+      <button
+        className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+        {...otherProps}
+      >
+        {children}
+      </button>
     </div>
   );
 };
