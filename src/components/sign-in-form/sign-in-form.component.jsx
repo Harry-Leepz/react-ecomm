ccc60a9266fb92ee16c4be06d6a62ff7273c1ef2
Harry <hdhillon478@gmail.com>
2022-06-19 18:57:47 +0100

    Update signIn functions to store user in UserContext

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import { UserContext } from "../../contexts/user.context";
 
 import {
   signInWithGooglePopup,
@@ -20,6 +22,9 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
+  // Instantiate the use of the sser context
+  const { setCurrentUser } = useContext(UserContext);
+
   // reset form
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -35,8 +40,8 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signInAuthWithEmailAndPassword(email, password);
-      console.log(response);
+      const { user } = await signInAuthWithEmailAndPassword(email, password);
+      setCurrentUser(user);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
