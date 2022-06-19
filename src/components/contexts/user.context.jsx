e5085dd4e5377c5f164189b130891711dfd2ecc0
Harry <hdhillon478@gmail.com>
2022-06-19 18:47:50 +0100

    Add user context explanation comment

--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,5 +1,11 @@
 import { createContext, useState } from "react";
 
+/* 
+  Core logic for this User content is to see if the user is signed in or not. 
+  If the User is signed in they will have auth credentials.
+  If not, then the values will be returned as NULL.
+*/
+
 // the actual value we want to access
 export const UserContext = createContext({
   currentUser: null,
