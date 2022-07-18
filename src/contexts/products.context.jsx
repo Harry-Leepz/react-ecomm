2a97cda0f7f24cf2edf11946fe97a983471c0278
Harry <hdhillon478@gmail.com>
2022-07-18 21:51:21 +0100

    Build out basic products context scaffold

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,3 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 import PRODUCTS from "../shop-data.json";
+
+export const ProductContext = createContext({
+  products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS);
+  const value = { products };
+
+  return <ProductsProvider value={value}>{children}</ProductsProvider>;
+};
