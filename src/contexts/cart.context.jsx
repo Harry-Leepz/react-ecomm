f83800642185b3428ef1037cd392a15efc62872f
Harry <hdhillon478@gmail.com>
2022-08-01 20:32:57 +0100

    Exapnd cart context to include cartItems and adding items to cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -3,10 +3,16 @@ import { useState, createContext } from "react";
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {};
+
   const value = { isCartOpen, setIsCartOpen };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
